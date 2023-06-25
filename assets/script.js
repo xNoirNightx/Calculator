d76d006751e2be80feba1f85c097fbb2bf6d41d2
Coral <cnight18@hotmail.com>
2023-06-25 17:08:10 -0300

    code is doubling #s

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,31 @@ const buttons = document.querySelectorAll(".btn-calculator");
 const equalButton = document.querySelector(".btn-primary");
 
 // listerns 
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      display.value += button.textContent;
+    });
+  });
+  
+  equalButton.addEventListener("click", () => {
+    try {
+      display.value = eval(display.value);
+    } catch (error) {
+      display.value = "Error";
+    }
+  });
 
-//  functions to calculate #s
-
-// save to local storage
+ 
+  buttons.forEach(button => button.addEventListener("click", () => display.value += button.textContent));
+  
+  equalButton.addEventListener("click", () => {
+    try {
+      const result = eval(display.value);
+      display.value = result;
+      localStorage.setItem("calculatorResult", result);
+    } catch (error) {
+      display.value = "Error";
+      localStorage.removeItem("calculatorResult");
+    }
+  });
+  
