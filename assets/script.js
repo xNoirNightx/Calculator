03fa99308a1a9a7a31d321d204deafa7b12d5c04
Coral <cnight18@hotmail.com>
2023-06-25 15:49:43 -0300

    working on js

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// all buttons
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".btn-calculator");
+const equalButton = document.querySelector(".btn-primary");
+
+// listerns 
+
+//  functions to calculate #s
+
+// save to local storage
