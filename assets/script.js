34eb96df152596a7583b4106cd62ea6fe11c8f76
Coral <cnight18@hotmail.com>
2023-06-26 18:24:07 -0300

    still scientific errors

--- assets/script.js
+++ assets/script.js
@@ -5,60 +5,74 @@ const equalButton = document.querySelector(".btn-primary");
 const clearButton = document.querySelector(".clear");
 const sqrtButton = document.querySelector(".sqrt");
 const exponentButton = document.querySelector(".exponent");
+const percentageButton = document.querySelector(".percentage");
 
-// listerns 
+// listeners
 buttons.forEach(button => {
-    button.removeEventListener("click", handleButtonClick); 
-    button.addEventListener("click", handleButtonClick);
-  });
-  
-// clear and equal functions to display *** add for all operations ***
-  equalButton.addEventListener("click", calculate);
-  clearButton.addEventListener("click", clearDisplay);
-  sqrtButton.addEventListener("click", calculateSquareRoot);
-  exponentButton.addEventListener("click", calculateExponent);
+  button.removeEventListener("click", handleButtonClick);
+  button.addEventListener("click", handleButtonClick);
+});
+
+// clear and equal functions to display
+equalButton.addEventListener("click", calculate);
+clearButton.addEventListener("click", clearDisplay);
+sqrtButton.addEventListener("click", calculateSquareRoot);
+exponentButton.addEventListener("click", calculateExponent);
+percentageButton.addEventListener("click", calculatePercentage);
 
 // clear
-  function clearDisplay() {
-    display.value = "";
-  }
+function clearDisplay() {
+  display.value = "";
+}
 
-//   scientific functions , parse ?
+// scientific functions
 function calculateSquareRoot() {
-    const value = parseFloat(display.value);
-    if (!isNaN(value)) {
-      const result = Math.sqrt(value);
-      display.value = result;
-      localStorage.setItem("calculatorResult", result);
-    }
+  const value = parseFloat(display.value);
+  if (!isNaN(value)) {
+    const result = Math.sqrt(value);
+    display.value = result;
+    localStorage.setItem("calculatorResult", result);
   }
-  
-  function calculateExponent() {
-    const value = parseFloat(display.value);
-    if (!isNaN(value)) {
-      const result = Math.pow(value, 2);
-      display.value = result;
-      localStorage.setItem("calculatorResult", result);
-    }
-  }
+}
 
-  
-//    add function to stop duplication
-  function handleButtonClick() {
-    const value = this.textContent;
-    display.value += value;
+function calculateExponent() {
+  const value = parseFloat(display.value);
+  if (!isNaN(value)) {
+    const result = Math.pow(value, 2);
+    display.value = result;
+    localStorage.setItem("calculatorResult", result);
   }
+}
 
-//   local storage
-  function calculate() {
-    try {
-      const result = eval(display.value);
-      display.value = result;
+function calculatePercentage() {
+  const expression = display.value;
+  const percentageValue = parseFloat(expression) / 100;
+  const result = percentageValue * evaluateExpression(expression);
+  display.value = result;
+  localStorage.setItem("calculatorResult", result);
+}
 
-      localStorage.setItem("calculatorResult", result);
-    } catch (error) {
-      display.value = "Error";
-      localStorage.removeItem("calculatorResult");
-    }
+// add function to stop duplication
+function handleButtonClick() {
+  const value = this.textContent;
+  display.value += value;
+}
+
+// local storage
+function calculate() {
+  try {
+    const result = evaluateExpression(display.value);
+    display.value = result;
+    localStorage.setItem("calculatorResult", result);
+  } catch (error) {
+    display.value = "Error";
+    localStorage.removeItem("calculatorResult");
   }
+}
+
+// evaluate 
+function evaluateExpression(expression) {
+  return Function(`"use strict"; return (${expression})`)();
+}
+
   
